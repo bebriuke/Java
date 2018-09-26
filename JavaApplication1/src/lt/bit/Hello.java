fe5af0812858f7ab7bb64c7556d764a1053a1bdd
LAPTOP-13U5S7B4\Jurate Valatkevicien <jurate.valatkeviciene@gmail.com>
2018-09-26 12:37:56 +0300

    Pirmas siuntimas

---
+++ JavaApplication1/src/lt/bit/Hello.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.bit;
+
+/**
+ *
+ * @author Jurate Valatkevicien
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ąčęėįšųū90-ž");
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        c = c*2;
+        System.out.println("c = " + c);
+    }
+    
+}
