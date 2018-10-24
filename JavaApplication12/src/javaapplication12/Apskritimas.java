edd7a156877f22aa8dbc22672cf0da8733a4e0f8
LAPTOP-13U5S7B4\Jurate Valatkevicien <jurate.valatkeviciene@gmail.com>
2018-10-24 14:23:34 +0300

    2018-10-24 kursas

---
+++ JavaApplication12/src/javaapplication12/Apskritimas.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+/**
+ *
+ * @author Jurate Valatkevicien
+ */
+public class Apskritimas {
+    private float x;
+    private float y;
+    private float r;
+    private static int kiekis = 0;
+   
+    public Apskritimas(float x, float y, float rr){
+        this.x = x;
+        this.y = y;
+        r = rr;
+        kiekis++;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getR() {
+        return r;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setR(float r) {
+        this.r = r;
+    }
+
+    @Override
+    public String toString() {
+        return "Apskritimas{" + "x=" + x + ", y=" + y + ", r=" + r + '}';
+    }
+    
+    public Apskritimas dvigubas(){
+        Apskritimas d =new Apskritimas(x, y, r);
+        d.setR(getR()*2);
+        return d;
+    }
+    
+    
+    public static float plotas(float r){
+        return 3.1415f*r*r;
+    }
+    
+    public static float plotas(Apskritimas a){  //statiniai metodai neturi "this"
+        return plotas(a.getR());
+    }
+    
+    
+     public float plotas(){
+        return plotas(this);
+    }
+
+    public static int getKiekis() {
+        return kiekis;
+    }
+     
+    
+}
--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -5,6 +5,8 @@
  */
 package javaapplication12;
 
+import java.lang.reflect.Method;
+
 /**
  *
  * @author Jurate Valatkevicien
@@ -22,6 +24,66 @@ public class JavaApplication12 {
             System.out.println(skaiciuoti(i, s)+j+k++);
         }
         System.out.println(s.r1 + " " + s.r2);
+        
+        //2018-10-23
+        
+        Apskritimas aps = new Apskritimas (10, 10, 25); //(1)
+        System.out.println(aps.toString());
+        
+        Apskritimas aps2 = new Apskritimas (15, 7, 3);//(2)
+        System.out.println(aps2.toString());
+        
+        System.out.println(aps2.toString().toUpperCase().length());
+        
+        Apskritimas aps3 = aps.dvigubas(); //(3)
+        System.out.println(aps3.toString());
+        /////////2018-10-26
+        System.out.println("*********************************");
+        
+        Apskritimas a = new Apskritimas(1, 2, 3); //(4)
+        System.out.println(a.getR());
+        System.out.println(a.toString());
+        Class c = a.getClass();
+        Method[] m = c.getMethods();
+        System.out.println(c.toString());
+        System.out.println("");
+        
+        System.out.println(c.getName());
+        System.out.println("");
+        
+        for(Method d : m){
+            System.out.println(d.getName());
+        }
+        
+        System.out.println("*********************************");
+        System.out.println("");
+        System.out.println(a.plotas(3));
+        System.out.println(a.plotas(a));  //nepatinka :( bet logika 64-65 eil.! :)
+        System.out.println(a.plotas()); //patinka :))))
+        System.out.println("");
+        System.out.println(Apskritimas.plotas(a));
+        System.out.println(Apskritimas.plotas(3));
+        
+        System.out.println("");
+        System.out.println(Apskritimas.getKiekis());
+        
+        a = new Apskritimas(3, 4, 5);
+        a = new Apskritimas(6, 7, 8);
+        new Apskritimas (9, 10, 11);
+        System.out.println(Apskritimas.getKiekis());  //statiniui objektui nereikia konteksto
+        System.out.println(aps3.getKiekis()); // jis pasako bendrą sukurtų objektų kiekį, todėl kreiptis su konkrečiu objektu nėra prasmės
+                
+        System.gc(); // iškviečiamas garbidž kolektorius;
+        
+        /* final klasė -- negalima sukurti klasės, paveldėtos nuo final
+        galima turėti kintamuosius final
+        final int a = 5;
+        a++; negalima
+        final veikia kaip konstanta, Java neturi konstantos rakto
+        
+        pasižiūrėti inner ir anonimines klases (arba vėliau :D
+        */
+        
     }
     
     public static int skaiciuoti(int i){
