2af2de59a488a1eb766b53096456f56dcd38e247
LAPTOP-13U5S7B4\Jurate Valatkevicien <jurate.valatkeviciene@gmail.com>
2018-12-11 16:01:34 +0200

    2018-12-11

--- JavaApplication18_mysql jdbc/src/javaapplication18_mysql/jdbc/JavaApplication18_mysqlJdbc.java
+++ JavaApplication18_mysql jdbc/src/javaapplication18_mysql/jdbc/JavaApplication18_mysqlJdbc.java
@@ -7,6 +7,7 @@ package javaapplication18_mysql.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,18 +25,77 @@ public class JavaApplication18_mysqlJdbc {
         Class.forName("com.mysql.cj.jdbc.Driver");//zipe kelias, prieš tai įkėlus į biblioteką (Projects -> Libraries dešiniu pelės, Add Library -> mysql-....jar
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/persons_scheme", "root", "root"); // jungimasis prie DB
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery("select * from persons");
+        String vardas = "Antanas";
+//        ResultSet rs = st.executeQuery("select * from persons");
+//        
+//        while (rs.next()){
+//            System.out.println(rs.getInt(1));
+//            System.out.println(rs.getString(2));
+//            System.out.println(rs.getString(3));
+//            System.out.println(rs.getDate(4));
+//            System.out.println(rs.getBigDecimal(5));
+//        }
+//        rs.close();
+
+
+        //st.execute("insert into persons(first_name, last_name) values('Jonas', 'Jonaitis')");
+        //st = con.createStatement(); 
+        //st.execute("delete from persons where id = 5");
+// Išrenka eilutes su konkrečiu vardu; ATSARGIAI! Įsilaužimas/duombazės praradimas su teisinga užklausa: String vardas = "Jonas';dropdatabase;select'"
+//        ResultSet rs = st.executeQuery("select * from persons where first_name = '"+vardas+"'");
+//        while (rs.next()){
+//            System.out.println(rs.getInt(1));
+//            System.out.println(rs.getString(2));
+//            System.out.println(rs.getString(3));
+//            System.out.println(rs.getDate(4));
+//            System.out.println(rs.getBigDecimal(5));
+//        }
         
-        while (rs.next()){
-            System.out.println(rs.getInt(1));
-            System.out.println(rs.getString(2));
-            System.out.println(rs.getString(3));
-            System.out.println(rs.getDate(4));
-            System.out.println(rs.getBigDecimal(5));
+
+//Šitas nebijo dropdatabase;
+//        PreparedStatement pst = con.prepareStatement("select * from persons where first_name = ?");  //kiekvienam klaustukui reikia suformuoti reikšmes;//parametrizuota užklausa
+//        pst.setString(1, vardas); //kievienam klaustukui siunčiamas parametras. kelintas klaustukas :)
+//        ResultSet rs = pst.executeQuery();
+//        while (rs.next()){
+//            System.out.println(rs.getInt(1));
+//            System.out.println(rs.getString(2));
+//            System.out.println(rs.getString(3));
+//            System.out.println(rs.getDate(4));
+//            System.out.println(rs.getBigDecimal(5));
+//        }
+        
+
+
+        System.out.println("Pasirinkite veiksmą:");
+        System.out.println("1 -- jeigu norite pamatyti visą lentelę;");
+        System.out.println("2 -- jeigu norite išsirinkti įrašus pagal vardą arba pavardę");
+        System.out.println("3 -- jeigu norite įrašyti naują darbuotoją į lentelę");
+        System.out.println("4 -- jeigu norite pakeisti darbuotojo atlyginimą");
+        System.out.println("5 -- jeigu norite pašalinti eilutę");
+        
+        int x = 1;
+        
+        
+        switch (x) {
+            case 1:  System.out.println("1");;
+                     break;
+            case 2:  System.out.println("2");
+                     break;
+            case 3:  System.out.println("3");
+                     break;
+            case 4:  System.out.println("4");
+                     break;
+            case 5:  System.out.println("5");
+                     break;
         }
-        rs.close();
+        
+        
         st.close();
         con.close();
     }
-    
+    //jeigu 1: parodyti sąrašą visų personų;
+    //jeigu 2: paprašyti vardo ir pavardės ir išvardinti tas eilutes, kur yra vardas arba pavardė;
+    //jeigu 3: paprašo vardo, pavardės, datos ir salarį ir įrašo tuos duomenis į duombazę;
+    //jeigu 4: paklausia ID, ir koks bus naujas to žmogaus atlyginimas ir updatina;
+    //jiegu 5: pakalusia ID ir ištrina tą eilutę;
 }
